983895c1994cf89c331ba3aa1d9b8d7ea61c0891
Quantum <aburorhambagadaniel@gmail.com>
2026-04-15 17:49:54 +0100

    added videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { AboutBook } from "./components/AboutBook";
@@ -7,16 +8,21 @@ import { AboutAuthor } from "./components/AboutAuthor";
 import { Testimonials } from "./components/Testimonials";
 import { FinalCTA } from "./components/FinalCTA";
 import { Footer } from "./components/Footer";
+import Videos from "./components/Videos";
+import Video2 from "./components/Video2";
 
 
 
 
 export default function App() {
+
   return (
     <div className="app">
       <Navbar />
       <Hero />
       <AboutBook />
+      <Videos />
+      <Video2 />
       <WhatYoullLearn />
       <AboutAuthor />
       <Testimonials />
